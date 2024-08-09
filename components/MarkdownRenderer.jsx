6b73cc649013e265f5076466f57c479d3bc35e67
daniel fisseha <developedbydanielfisseha@gmail.com>
2024-08-09 14:25:12 +0300

    nest children between the opening and closing tags.  react/no-children-prop

--- components/MarkdownRenderer.jsx
+++ components/MarkdownRenderer.jsx
@@ -2,9 +2,8 @@ import ReactMarkdown from "react-markdown";
 
 export default function MarkdownRenderer({ markdown }) {
   return (
-    <ReactMarkdown
-      className="max-w-screen-md mx-auto px-4 py-8 text-white"
-      children={markdown}
-    />
+    <ReactMarkdown className="max-w-screen-md mx-auto px-4 py-8 text-white">
+      markdown
+    </ReactMarkdown>
   );
 }
